5e3343078883872db343e36d119862299eb53146
Alex Ulanov <flame9172@gmail.com>
2025-07-14 10:38:08 +0000

    Adding withdraw and deposit methods

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -10,63 +10,78 @@ public class DataSource {
   public static Connection connect() {
     String db_file = "jdbc:sqlite:resources/bank.db";
     Connection connection = null;
-    try{
+    try {
       connection = DriverManager.getConnection(db_file);
       System.out.println("We are connected");
-    } catch (SQLException e){
+    } catch (SQLException e) {
       e.printStackTrace();
     }
 
     return connection;
-    
+
   }
 
-  public static Customer getCustomer (String username){
+  public static Customer getCustomer(String username) {
     String sql = "select * from customers where username = ?";
     Customer customer = null;
 
-    try(Connection connection = connect();
-        PreparedStatement statement = connection.prepareStatement(sql)){
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
 
-          statement.setString(1, username);
-          try (ResultSet resultSet = statement.executeQuery()){
-            customer = new Customer(
-              resultSet.getInt("id"),
-              resultSet.getString("name"),
-              resultSet.getString("username"),
-              resultSet.getString("password"),
-              resultSet.getInt("account_id"));
+      statement.setString(1, username);
+      try (ResultSet resultSet = statement.executeQuery()) {
+        customer = new Customer(
+            resultSet.getInt("id"),
+            resultSet.getString("name"),
+            resultSet.getString("username"),
+            resultSet.getString("password"),
+            resultSet.getInt("account_id"));
 
-          }
+      }
 
-    } catch(SQLException e){
+    } catch (SQLException e) {
       e.printStackTrace();
     }
     return customer;
   }
 
-  public static Account getAccount(int accountId){
+  public static Account getAccount(int accountId) {
     String sql = "select * from accounts where id = ?";
     Account account = null;
 
     try (Connection connection = connect();
-          PreparedStatement statement = connection.prepareStatement(sql)){
-            statement.setInt(1, accountId);
-            try (ResultSet resultSet = statement.executeQuery()){
-              account = new Account(
-                resultSet.getInt("id"),
-                resultSet.getString("type"),
-                resultSet.getDouble("balance"));
-          }
-        } catch (SQLException e){
-          e.printStackTrace();
-        }
-        return account;
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setInt(1, accountId);
+      try (ResultSet resultSet = statement.executeQuery()) {
+        account = new Account(
+            resultSet.getInt("id"),
+            resultSet.getString("type"),
+            resultSet.getDouble("balance"));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return account;
 
   }
 
+  public static void updateAccountBalance(int accountId, double balance) {
+    String sql = "update accounts set balance = ? where id = ?";
+    try (
+        Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql);) {
+      statement.setDouble(1, balance);
+      statement.setInt(2, accountId);
+
+      statement.executeUpdate();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
   public static void main(String[] args) {
-   Customer customer = getCustomer("ohalbardv@booking.com");
+    Customer customer = getCustomer("ohalbardv@booking.com");
     System.out.println(customer.getName());
     Account account = getAccount(13053);
     System.out.println(account.getBalance());
